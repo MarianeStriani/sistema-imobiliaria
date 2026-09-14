a0bc3a17e4177e4465fb7328a3f9cc346d948113
MarianeStriani <marianeisabela@gmail.com>
2026-09-13 22:37:11 -0300

    Atualizar o page.js

--- page.js
+++ page.js
@@ -4,18 +4,12 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <main style={{ padding: "40px" }}>
+    <main>
       <h1>Sistema de Administração de Imóveis</h1>
 
-      <p>
-        Bem-vindo ao sistema de gestão imobiliária.
-      </p>
-
-      <div style={{ marginTop: "30px" }}>
-        <Link href="/dashboard">
-          <button>Entrar no Dashboard</button>
-        </Link>
-      </div>
+      <Link href="/dashboard">
+        <button>Entrar no Dashboard</button>
+      </Link>
     </main>
   )
 }
