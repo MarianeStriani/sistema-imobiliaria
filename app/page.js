f4ba68e72a507c459ae1faafea3ebeebc9e7d304
MarianeStriani <marianeisabela@gmail.com>
2026-09-13 22:41:39 -0300

    Criar page.js

---
+++ app/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Sistema Imobiliário</h1>
+
+      <p>Sistema de administração de imóveis</p>
+
+      <a href="/dashboard">
+        Acessar Dashboard
+      </a>
+    </main>
+  )
+}
