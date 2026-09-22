49d5844d941c3f4b5595a9a1f8df857828a7b897
MarianeStriani <marianeisabela@gmail.com>
2026-09-22 00:08:35 -0300

    Atualizar o page.js

--- app/clientes/page.js
+++ app/clientes/page.js
@@ -377,8 +377,6 @@ export default function Clientes() {
         </div>
       </div>
 
- <div className="container-fluid py-4">
-
 {/* CABEÇALHO */}
       <div className="d-flex flex-wrap justify-content-between align-items-center mb-4">
         <div>
