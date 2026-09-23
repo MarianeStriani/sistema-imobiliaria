bb111997a4c2be44d5bd9d2acbd9b7bd891ace64
MarianeStriani <marianeisabela@gmail.com>
2026-09-23 10:36:36 -0300

    Atualizar o layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,17 @@
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import LogoutButton from "./components/LogoutButton";
 
 export const metadata = {
-title: "Sistema Imobiliária",
-description: "Sistema de administração de imóveis",
-}
+  title: "Sistema Imobiliária",
+  description: "Sistema de administração de imóveis",
+};
 
 export default function RootLayout({ children }) {
-return (
-<html lang="pt-BR">
-<body>{children}</body>
-</html>
-)
+  return (
+    <html lang="pt-BR">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
 }
